80e53676e94819005d92b7e4f8f488cc2b073fd0
Heinrich Kuettler <heinrich.kuettler@gmail.com>
2015-03-08 19:57:04 +0100

    Added Android project
    $ android create project --target android-21 --name MathApp --path MathApp \
     --activity MathActivity --package org.kuettler.mathapp

---
+++ MathApp/src/org/kuettler/mathapp/MathActivity.java
@@ -0,0 +1,15 @@
+package org.kuettler.mathapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MathActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
